12e9c293c94e3201ff1b1b6b99b46aadf4a30319
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-07-14 07:31:44 +0600

    Some Text Added.

--- src/pages/home/RealtimeFloodAffectDetection.tsx
+++ src/pages/home/RealtimeFloodAffectDetection.tsx
@@ -23,10 +23,11 @@ const RealtimeFloodAffectDetection = () => {
         <div className="w-full md:w-1/2 flex flex-col justify-center items-start p-4">
           <h2 className="text-2xl md:text-3xl font-bold mb-4">Our Mission</h2>
           <p className="text-base md:text-lg mb-2">
-            Discover EnviroMonitor: Empowering disaster readiness with
-            cutting-edge AI technology. Experience real-time weather insights,
-            live drone footage for crisis response, and predictive analytics
-            using advanced machine learning. Stay informed, stay prepared.
+            Real-time flood-affected people detection leverages machine learning
+            (ML) techniques to identify and monitor individuals impacted by
+            flood events. This advanced approach integrates various data sources
+            and ML models to provide timely and accurate information, which is
+            crucial for effective disaster response and management.
           </p>
         </div>
         <div className="w-full md:w-1/2 flex justify-center md:justify-start">
--- src/pages/home/posts/PeopleDetection.tsx
+++ src/pages/home/posts/PeopleDetection.tsx
@@ -24,10 +24,7 @@ const PeopleDetection = () => {
         <div className="w-full md:w-1/2 flex flex-col justify-center -mt-6 items-start p-4">
           <h2 className="text-2xl md:text-3xl font-bold mb-4">Our Mission</h2>
           <p className="text-base md:text-lg mb-2">
-            Discover EnviroMonitor: Empowering disaster readiness with
-            cutting-edge AI technology. Experience real-time weather insights,
-            live drone footage for crisis response, and predictive analytics
-            using advanced machine learning. Stay informed, stay prepared.
+          Real-time flood-affected people detection leverages machine learning (ML) techniques to identify and monitor individuals impacted by flood events. This advanced approach integrates various data sources and ML models to provide timely and accurate information, which is crucial for effective disaster response and management.
           </p>
         </div>
         <div className="w-full md:w-1/2 flex justify-center md:justify-start">
